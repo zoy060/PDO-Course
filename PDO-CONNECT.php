2326ee5115950c59c935924cf45c2d8647539dd3
zoy060 <mizan258173@gmail.com>
2023-09-07 20:01:33 +0600

    PDO CONNECT Success

--- PDO-CONNECT.php
+++ PDO-CONNECT.php
@@ -1,4 +1,17 @@
 <?php
 
+//host, dbname , user, password
 
+$host = "localhost";
+$dbname = "zoya"; //your database name here
+$user ="root" ; 	//username of your mysql server
+$password=""; 	  	  //password for the username above
+
+$dbh = new PDO("mysql:host=$host;dbname=$dbname","$user","$password");
+
+if($dbh == true) {
+    echo 'Connection Successful';
+}else{
+    echo'Error in connection'. $e->getMessage();
+}
 
